8f4373cc0e3a5f9bdc76cea4e8fa741f60ecf62b
Lukas Innig <lukas.innig@datarobot.com>
2024-05-09 11:35:22 +0100

    adding missing drx sources

---
+++ src/datarobotx/idp/common/credentials_hooks.py
@@ -0,0 +1,51 @@
+#
+# Copyright 2024 DataRobot, Inc. and its affiliates.
+#
+# All rights reserved.
+#
+# DataRobot, Inc.
+#
+# This is proprietary source code of DataRobot, Inc. and its
+# affiliates.
+#
+# Released under the terms of DataRobot Tool and Utility Agreement.
+# https://www.datarobot.com/wp-content/uploads/2021/07/DataRobot-Tool-and-Utility-Agreement.pdf
+
+from typing import Any, Dict
+
+try:
+    from kedro.framework.hooks import hook_impl
+    from kedro.io import DataCatalog
+except ImportError as e:
+    raise ImportError("Consider including kedro in your project requirements`") from e
+
+from datarobotx.idp.common.handle_io import get_feed_dict
+
+
+class CredentialsHooks:
+    """Include credentials in the catalog so they can be used by nodes."""
+
+    @hook_impl
+    def after_catalog_created(
+        self,
+        catalog: DataCatalog,
+        conf_catalog: Dict[str, Any],
+        conf_creds: Dict[str, Any],
+        save_version: str,
+        load_versions: Dict[str, str],
+    ) -> None:
+        """Validate credentials and add them to the catalog for node consumption."""
+        self.validate_credentials(conf_creds)
+
+        creds = get_feed_dict(conf_creds, copy_dict_as=None, key_prefix="params:credentials.")
+        catalog.add_feed_dict(creds)
+
+    @staticmethod
+    def validate_credentials(conf_creds: Dict[str, Any]) -> None:
+        """Confirm credentials appear to be valid and functional."""
+        import datarobot as dr
+
+        dr_creds = conf_creds["datarobot"]
+
+        client = dr.Client(token=dr_creds["api_token"], endpoint=dr_creds["endpoint"])  # type: ignore
+        client.get("version/")
